0edc014f6df06bf48710b6db7c96db208e055990
hassana123 <hassanaabdll1@gmail.com>
2024-07-26 06:53:04 -0700

    updates

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -11,10 +11,10 @@
 //   const [diagnosing, setDiagnosing] = useState(false);
 
 //   const handleFileChange = async (e) => {
-//     setResult(null)
+//     setResult(null);
 //     setUploading(true);
 //     const file = e.target.files[0];
-//     setImage(file)
+//     setImage(file);
 //     if (file) {
 //       try {
 //         const storageRef = ref(storage, `photos/${file.name}`);
@@ -31,23 +31,47 @@
 //   };
 
 //   const handleImageDiagnose = async (e) => {
-//     setResult(null)
+//     setResult(null);
 //     e.preventDefault();
 //     setDiagnosing(true);
 //     const reader = new FileReader();
 //     reader.onload = async (e) => {
 //       const base64Image = e.target.result.split(",")[1];
 //       try {
-//         const response = await axios.post("https://susya.onrender.com", {
-//           image: base64Image,
+//         const response = await axios.post(
+//           "https://crop.kindwise.com/api/v1/identification",
+//           {
+//             images: [base64Image],
+//           },
+//           {
+//             headers: {
+//               "Content-Type": "application/json",
+//               "Api-Key": import.meta.env.VITE_KINDWISE_API_KEY,
+//             },
+//           }
+//         );
+//         console.log(response.data); // Log the full response data
+  
+//         // Extracting the relevant information with safety checks
+//         const diseaseSuggestions = response.data?.result?.disease?.suggestions || [];
+//         const cropSuggestions = response.data?.result?.crop?.suggestions || [];
+//         const crop = cropSuggestions[0] || {};  // Take the first suggestion if available
+//         const diseases = diseaseSuggestions.map(suggestion => ({
+//           name: suggestion.name || "Unknown disease",
+//           scientificName: suggestion.scientific_name || "Unknown scientific name",
+//           probability: (suggestion.probability * 100).toFixed(2) + '%',  // Convert to percentage
+//           treatment: suggestion.details?.treatment || "No treatment available"
+//         }));
+  
+//         setResult({
+//           diseases: diseases,
+//           plant: crop.name || "Unknown plant",
+//           suggestions: cropSuggestions.map(suggestion => ({
+//             name: suggestion.name || "Unknown plant",
+//             probability: (suggestion.probability * 100).toFixed(2) + '%'  // Convert to percentage
+//           })),
+//           status: diseases.length > 0 ? "Infected" : "Healthy",
 //         });
-//         console.log(response.data);
-//         const { disease, plant, remedy } = response.data;
-//         setResult({ disease, plant, remedy });
-
-//         // Determine whether the plant is infected or healthy
-//         const status = disease ? "Infected" : "Healthy";
-//         setResult((prevResult) => ({ ...prevResult, status }));
 //       } catch (error) {
 //         console.error("Error diagnosing image:", error);
 //       } finally {
@@ -59,7 +83,11 @@
 
 //   // download report as text file
 //   const downloadReport = () => {
-//     const blob = new Blob([`Status: ${result.status}\nDisease: ${result.disease}\nPlant: ${result.plant}\nRemedy: ${result.remedy}`], { type: 'text/plain;charset=utf-8' });
+//     const report = `Status: ${result.status}\nPlant: ${result.plant}\nDiseases:\n` +
+//       result.diseases.map(disease => 
+//         `- Name: ${disease.name}\n  Scientific Name: ${disease.scientificName}\n  Probability: ${disease.probability}\n  Remedy: ${disease.treatment}`
+//       ).join("\n");
+//     const blob = new Blob([report], { type: 'text/plain;charset=utf-8' });
 //     const url = URL.createObjectURL(blob);
 //     const a = document.createElement('a');
 //     a.href = url;
@@ -70,11 +98,9 @@
 
 //   // URL for additional tips based on the disease
 //   const getTipsUrl = (disease) => {
-//     if (disease) {
-//       return `https://www.apsnet.org/edcenter/disimpactmngmnt/topc/EpidemiologyTemporal/Pages/ManagementStrategies.aspx`; // Replace with actual URLs
-//     } else {
-//       return 'https://www.elitetreecare.com/2021/02/maintaining-plant-health-tips/'; 
-//     }
+//     return disease
+//       ? 'https://www.apsnet.org/edcenter/disimpactmngmnt/topc/EpidemiologyTemporal/Pages/ManagementStrategies.aspx'  // Replace with actual URLs
+//       : 'https://www.elitetreecare.com/2021/02/maintaining-plant-health-tips/';
 //   };
 
 //   return (
@@ -91,6 +117,7 @@
 //         <p className="text-[18px] font-[400]">
 //           Based on the image you provided, here are the details of your crop's health status.
 //         </p>
+      
 //         <div
 //           style={{
 //             backgroundImage: imageURL ? `url(${imageURL})` : "none",
@@ -104,24 +131,25 @@
 //             style={{ display: "none" }}
 //             onChange={handleFileChange}
 //           />
-//           <label
-//             htmlFor="fileUpload"
-//             className="border cursor-pointer w-[40%] mx-auto text-center">
-//             <span className="mx-auto block border border-[#90BFA8] rounded-[6px] text-[#90BFA8] shadow-lg py-4 px6">
-//               {uploading
-//                 ? "Uploading..."
-//                 : imageURL
-//                 ? "Change Image"
-//                 : "Upload Image"}
-//             </span>
-//           </label>
 //         </div>
+        
+//        <div className="items-center flex md:w-[40%]">
 //         <button
 //           onClick={handleImageDiagnose}
 //           className="font-roboto rounded-lg text-white px-10 py-3 bg-primary"
 //           disabled={diagnosing}>
 //           {diagnosing ? "Diagnosing..." : "Diagnose"}
 //         </button>
+//         <label
+//             htmlFor="fileUpload"
+//             className="border cursor-pointer mx-auto text-center mx-auto block border-[#90BFA8] rounded-[6px] text-[#90BFA8] shadow-sm py-3 px-6">
+//             {uploading
+//                 ? "Uploading..."
+//                 : imageURL
+//                 ? "Change Image"
+//                 : "Upload Image"}
+//           </label>
+//        </div>
 //       </div>
 //       {result && (
 //         <div className="mx-5 p-5 rounded-lg shadow-lg">
@@ -139,37 +167,48 @@
 //               </small>
 //             </li>
 //             <li className="space-y-1">
-//               <p className="text-lg inline font-bold">Disease</p>
-//               <small className="block ml-5">{result.disease ? result.disease : "None"}</small>
-//             </li>
-//             <li className="space-y-1">
 //               <p className="text-lg inline font-bold">Plant</p>
 //               <small className="block ml-5">{result.plant}</small>
 //             </li>
 //             <li className="space-y-1">
-//               <p className="text-lg inline font-bold">Remedy</p>
-//               <small className="block ml-5">{result.remedy}</small>
+//               <p className="text-lg inline font-bold">Possible Plants</p>
+//               {result.suggestions.map((sug, index) => (
+//                 <small key={index} className="block ml-5">
+//                   {sug.name} ({sug.probability})
+//                 </small>
+//               ))}
+//             </li>
+//             <li>  <p className="text-lg inline font-bold">Possible Diseases</p> 
+//             {result.status === "Infected" && (
+//               <div className="grid grid-cols-1 md:grid-cols-2 my-3 gap-4">
+//                 {result.diseases.map((disease, index) => (
+//                   <div key={index} className="space-y-1 border p-3 rounded-lg shadow-sm">
+//                     <p className="text-lg font-bold">Disease {index + 1}</p>
+//                     <small className="block">Name: {disease.name}</small>
+//                     <small  className="block"> Scientific Name: {disease.scientificName}</small>
+//                     <small  className="block">Probability: {disease.probability}</small>
+//                     <small  className="block">Remedy: {disease.treatment}</small>
+//                   </div>
+//                 ))}
+//               </div>
+//             )}
 //             </li>
 //           </ol>
 //           {result.status === "Infected" && (
 //             <div className="p-4 bg-red-100 border border-red-400 rounded-md">
 //               <h4 className="font-bold text-red-700">Infection Alert!</h4>
-//               <p>{result.remedy}</p>
+//               <p>{result.diseases[0].treatment}</p>
 //             </div>
 //           )}
-//           <div className="mt-5 flex space-x-4">
+//           <div className="mt-5 md:flex space-y-5 mx-auto mx:space-x-4">
 //             <button
 //               onClick={downloadReport}
-//               className="px-10 py-4 bg-primary font-bold text-white rounded-md shadow-md"
-//             >
+//               className="bg-secondary block w-[50%] md:w-auto text-white py-3 px-6 rounded-lg font-roboto">
 //               Download Report
 //             </button>
 //             <a
-//               href={getTipsUrl(result.disease)}
-//               className="px-6 py-2 bg-white text-primary flex items-center  rounded-md border border-primary rounded-md shadow-md"
-//               target="_blank"
-//               rel="noopener noreferrer"
-//             >
+//               href={getTipsUrl(result.status === "Infected")}
+//               className="bg-green-600 w-[50%] md:w-auto block text-center text-white py-3 px-6 rounded-lg font-roboto">
 //               Get More Tips
 //             </a>
 //           </div>
@@ -181,10 +220,12 @@
 
 // export default Upload;
 
+
 import React, { useState } from "react";
 import axios from "axios";
 import { storage } from "../../firebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import AIChatbot from "./AIChatbot"; // Import the AIChatbot component
 
 const Upload = () => {
   const [image, setImage] = useState(null);
@@ -229,29 +270,31 @@ const Upload = () => {
           {
             headers: {
               "Content-Type": "application/json",
-              "Api-Key": "OgxWQ2QUfQ2WZ7br07Ujb4hjQsSFlCRnPb2PHWBQBUT4b1wKjo", // Replace with your actual API key
+              "Api-Key": import.meta.env.VITE_KINDWISE_API_KEY,
             },
           }
         );
         console.log(response.data); // Log the full response data
   
-        // Extracting the relevant information with safety checks
-        const disease = response.data?.result?.disease;
-        const crop = response.data?.result?.crop;
-        const treatment = disease?.suggestions ? disease.suggestions[0]?.treatment : "No treatment available";
+        const diseaseSuggestions = response.data?.result?.disease?.suggestions || [];
+        const cropSuggestions = response.data?.result?.crop?.suggestions || [];
+        const crop = cropSuggestions[0] || {};  // Take the first suggestion if available
+        const diseases = diseaseSuggestions.map(suggestion => ({
+          name: suggestion.name || "Unknown disease",
+          scientificName: suggestion.scientific_name || "Unknown scientific name",
+          probability: (suggestion.probability * 100).toFixed(2) + '%',  // Convert to percentage
+          treatment: suggestion.details?.treatment || "No treatment available"
+        }));
   
-        if (disease && crop) {
-          setResult({
-            disease: disease.name || "Unknown disease",
-            plant: crop.name || "Unknown plant",
-            remedy: treatment || "No treatment available",
-            status: "Infected",
-          });
-        } else {
-          setResult({
-            status: "Healthy",
-          });
-        }
+        setResult({
+          diseases: diseases,
+          plant: crop.name || "Unknown plant",
+          suggestions: cropSuggestions.map(suggestion => ({
+            name: suggestion.name || "Unknown plant",
+            probability: (suggestion.probability * 100).toFixed(2) + '%'  // Convert to percentage
+          })),
+          status: diseases.length > 0 ? "Infected" : "Healthy",
+        });
       } catch (error) {
         console.error("Error diagnosing image:", error);
       } finally {
@@ -260,11 +303,13 @@ const Upload = () => {
     };
     reader.readAsDataURL(image);
   };
-  
 
-  // download report as text file
   const downloadReport = () => {
-    const blob = new Blob([`Status: ${result.status}\nDisease: ${result.disease}\nPlant: ${result.plant}\nRemedy: ${result.remedy}`], { type: 'text/plain;charset=utf-8' });
+    const report = `Status: ${result.status}\nPlant: ${result.plant}\nDiseases:\n` +
+      result.diseases.map(disease => 
+        `- Name: ${disease.name}\n  Scientific Name: ${disease.scientificName}\n  Probability: ${disease.probability}\n  Remedy: ${disease.treatment}`
+      ).join("\n");
+    const blob = new Blob([report], { type: 'text/plain;charset=utf-8' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
@@ -273,13 +318,10 @@ const Upload = () => {
     URL.revokeObjectURL(url);
   };
 
-  // URL for additional tips based on the disease
   const getTipsUrl = (disease) => {
-    if (disease) {
-      return `https://www.apsnet.org/edcenter/disimpactmngmnt/topc/EpidemiologyTemporal/Pages/ManagementStrategies.aspx`; // Replace with actual URLs
-    } else {
-      return 'https://www.elitetreecare.com/2021/02/maintaining-plant-health-tips/';
-    }
+    return disease
+      ? 'https://www.apsnet.org/edcenter/disimpactmngmnt/topc/EpidemiologyTemporal/Pages/ManagementStrategies.aspx'  // Replace with actual URLs
+      : 'https://www.elitetreecare.com/2021/02/maintaining-plant-health-tips/';
   };
 
   return (
@@ -296,6 +338,7 @@ const Upload = () => {
         <p className="text-[18px] font-[400]">
           Based on the image you provided, here are the details of your crop's health status.
         </p>
+      
         <div
           style={{
             backgroundImage: imageURL ? `url(${imageURL})` : "none",
@@ -309,25 +352,27 @@ const Upload = () => {
             style={{ display: "none" }}
             onChange={handleFileChange}
           />
+        </div>
+        
+        <div className="items-center block md:space-y-0 space-y-2 md:flex md:w-[65%] lg:w-[55%]">
+          <button
+            onClick={handleImageDiagnose}
+            className="font-roboto rounded-lg w-full  md:w-[50%] text-white px-10 py-3 bg-primary"
+            disabled={diagnosing}>
+            {diagnosing ? "Diagnosing..." : "Diagnose"}
+          </button>
           <label
             htmlFor="fileUpload"
-            className="border cursor-pointer w-[40%] mx-auto text-center">
-            <span className="mx-auto block border border-[#90BFA8] rounded-[6px] text-[#90BFA8] shadow-lg py-4 px6">
-              {uploading
+            className="border cursor-pointer mx-auto text-center mx-auto block border-[#90BFA8] rounded-[6px] text-[#90BFA8] shadow-sm py-3 px-6">
+            {uploading
                 ? "Uploading..."
                 : imageURL
                 ? "Change Image"
                 : "Upload Image"}
-            </span>
           </label>
         </div>
-        <button
-          onClick={handleImageDiagnose}
-          className="font-roboto rounded-lg text-white px-10 py-3 bg-primary"
-          disabled={diagnosing}>
-          {diagnosing ? "Diagnosing..." : "Diagnose"}
-        </button>
       </div>
+
       {result && (
         <div className="mx-5 p-5 rounded-lg shadow-lg">
           <h3 className="text-[20px] md:text-[25px] font-bold mb-4">Diagnosis Result</h3>
@@ -344,37 +389,50 @@ const Upload = () => {
               </small>
             </li>
             <li className="space-y-1">
-              <p className="text-lg inline font-bold">Disease</p>
-              <small className="block ml-5">{result.disease ? result.disease : "None"}</small>
-            </li>
-            <li className="space-y-1">
               <p className="text-lg inline font-bold">Plant</p>
               <small className="block ml-5">{result.plant}</small>
             </li>
             <li className="space-y-1">
-              <p className="text-lg inline font-bold">Remedy</p>
-              <small className="block ml-5">{result.remedy}</small>
+              <p className="text-lg inline font-bold">Possible Plants</p>
+              {result.suggestions.map((sug, index) => (
+                <small key={index} className="block ml-5">
+                  {sug.name} ({sug.probability})
+                </small>
+              ))}
+            </li>
+            <li>
+              <p className="text-lg inline font-bold">Possible Diseases</p> 
+              {result.status === "Infected" && (
+                <div className="grid grid-cols-1 md:grid-cols-2 my-3 gap-4">
+                  {result.diseases.map((disease, index) => (
+                    <div key={index} className="space-y-1 border p-3 rounded-lg shadow-sm">
+                      <p className="text-lg font-bold">Disease {index + 1}</p>
+                      <small className="block">Name: {disease.name}</small>
+                      <small className="block">Scientific Name: {disease.scientificName}</small>
+                      <small className="block">Probability: {disease.probability}</small>
+                      <small className="block">Remedy: {disease.treatment}</small>
+                    </div>
+                  ))}
+                </div>
+              )}
             </li>
           </ol>
-          {result.status === "Infected" && (
+          {/* {result.status === "Infected" && (
             <div className="p-4 bg-red-100 border border-red-400 rounded-md">
               <h4 className="font-bold text-red-700">Infection Alert!</h4>
-              <p>{result.remedy}</p>
+              <p>{result.diseases[0].treatment}</p>
             </div>
-          )}
+          )} */}
+          <AIChatbot diseases={result.diseases} /> {/* Integrate AIChatbot component */}
           <div className="mt-5 md:flex space-y-5 mx-auto mx:space-x-4">
             <button
               onClick={downloadReport}
-              className="px-10 py-4 bg-primary font-bold text-white rounded-md shadow-md"
-            >
+              className="bg-secondary block w-[50%] md:w-auto text-white py-3 px-6 rounded-lg font-roboto">
               Download Report
             </button>
             <a
-              href={getTipsUrl(result.disease)}
-              className="px-6 py-2 bg-white text-primary flex items-center  rounded-md border border-primary rounded-md shadow-md"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+              href={getTipsUrl(result.status === "Infected")}
+              className="bg-green-600 w-[50%] md:w-auto block text-center text-white py-3 px-6 rounded-lg font-roboto">
               Get More Tips
             </a>
           </div>
